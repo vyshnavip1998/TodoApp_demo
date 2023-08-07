4201090aa9097ad41a811234ff02117faaa6ee45
vyshnavip1998 <118480225+vyshnavip1998@users.noreply.github.com>
2023-08-07 22:15:13 +0530

    commit

---
+++ todoproject/todoapp/views.py
@@ -0,0 +1,86 @@
+# from django.core.checks import messages
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+
+from .forms import TodoForm
+from .models import Task
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+
+
+# CLASS GENERIC
+class TaskListView(ListView):
+    model = Task
+    template_name = 'home.html'
+    context_object_name = 'tasks'
+
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'detail.html'
+    context_object_name = 'task'
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = 'update.html'
+    context_object_name = 'task'
+    fields = ('name', 'priority', 'date')
+
+    def get_success_url(self):
+        return reverse_lazy('cbvdetail', kwargs={'pk': self.object.id})
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'delete.html'
+    success_url = reverse_lazy('cbvhome')
+
+
+
+# Create your views here.
+
+def add(request):
+    tasks = Task.objects.all()
+    if request.method == 'POST':
+        name = request.POST.get('task', '')
+        priority = request.POST.get('priority', '')
+        date = request.POST.get('date', '')
+        task = Task(name=name, priority=priority, date=date)
+        task.save()
+        messages.info(request, "ToDo added in the list !!!")
+    # else:
+    #     task = Task.objects.none()
+    #
+    #     messages.warning(request, "Your ToDo is EMPTY !!")
+
+    return render(request, "home.html", {'tasks': tasks})
+
+
+def delete(request, taskid):
+    task = Task.objects.get(id=taskid)
+    if request.method == 'POST':
+        task.delete()
+        messages.info(request, "ToDo deleted !!!")
+        return redirect('/')
+
+    return render(request, 'delete.html')
+
+
+def update(request, id):
+    task = Task.objects.get(id=id)
+    f = TodoForm(request.POST or None, instance=task)
+    if f.is_valid():
+        f.save()
+        messages.info(request, "ToDo Updated !!!")
+        return redirect('/')
+    return render(request, 'edit.html', {'f': f, 'task': task})
+# def details(request):
+#     task=Task.objects.all()
+#     return render(request,'detail.html',{'task':task})
